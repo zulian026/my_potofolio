0da736c8ac777f1d1cfa8bfa7f4033bba12f93db
zulian026 <zulianalhisyam@gmail.com>
2025-04-18 17:20:46 +0700

    two

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { ghPages } from "vite-plugin-gh-pages"; // ← tambahkan ini
 
 // https://vite.dev/config/
 export default defineConfig({
